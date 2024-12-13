e80ecdb6adb3c681fc4c51e23abb96230eff11c8
pengtao.geng <pengtao.geng>
2024-12-13 14:38:24 +0800

    add test

---
+++ learn/tests/test_decorator_django.py
@@ -0,0 +1,24 @@
+from django.utils.decorators import method_decorator
+
+
+# 装饰器函数
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("装饰器执行前")
+        for arg in args:
+            print(arg)
+        for key, value in kwargs.items():
+            print(f"{key}: {value}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@method_decorator(my_decorator)
+def my_function(arg):
+    """这个是我们的目标要执行的函数"""
+    print("function执行了,参数是:" + arg)
+
+
+# 调用函数
+my_function("xxxxxxxxxxxxxx")
