32bccd5175dcfb3cd1e09302dc51b38c6433fe7d
gpengtao <513023768@qq.com>
2024-12-19 00:01:32 +0800

    modify pb

--- marathon/views.py
+++ marathon/views.py
@@ -14,7 +14,7 @@ def my_marathon(request):
             'event': '半马',
             'result': '完赛',
             'net_time': '1:53:01',
-            'notes': '首半马'
+            'notes': '首半马',
         },
         {
             'date': '2024-10-20',
@@ -22,15 +22,15 @@ def my_marathon(request):
             'event': '半马',
             'result': '完赛',
             'net_time': '1:46:47',
-            'notes': '半马 PB'
+            'notes': '首次山东跑马',
         },
         {
             'date': '2024-11-03',
             'name': '2024临沂马拉松',
             'event': '半马',
-            'result': '未参加',
-            'net_time': '-',
-            'notes': '-'
+            'result': '完赛',
+            'net_time': '2:24:29',
+            'notes': '嗯啊我一点都不累🐶',
         },
         {
             'date': '2024-12-29',
@@ -38,7 +38,7 @@ def my_marathon(request):
             'event': '17公里',
             'result': '还未开始',
             'net_time': '-',
-            'notes': '-'
+            'notes': '-',
         },
         {
             'date': '2025-01-01',
@@ -46,17 +46,28 @@ def my_marathon(request):
             'event': '10公里',
             'result': '还未开始',
             'net_time': '-',
-            'notes': '-'
+            'notes': '-',
         }
     ]
 
     # 按日期排序，正序
     races.sort(key=lambda x: x['date'])
 
+    # 找出半马和全马的最好成绩
+    tmp_pbs = {'半马': None, '全马': None}
+    for race in races:
+        if race['event'] in ['半马', '全马'] and race['result'] == '完赛':
+            event_type = race['event']
+            if tmp_pbs[event_type] is None or (race['net_time'] < tmp_pbs[event_type]['net_time']):
+                # 如果之前有PB，先把它设为False
+                if tmp_pbs[event_type]:
+                    tmp_pbs[event_type]['pb'] = False
+                tmp_pbs[event_type] = race
+                race['pb'] = True
+
     # 按年份分组
     year_group = {}
     for race in races:
-        # 从日期中提取年份
         year = race['date'][:4]
         if year not in year_group:
             year_group[year] = []
