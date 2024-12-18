da47b8f194f425350e4abcadc92201464cdee19a
gpengtao <513023768@qq.com>
2024-12-18 23:38:11 +0800

    增加按照年分组

--- marathon/views.py
+++ marathon/views.py
@@ -9,7 +9,6 @@ def hello_world(request):
 def my_marathon(request):
     races = [
         {
-            'year': '2024',
             'date': '2024-09-08',
             'name': '2024新河半程马拉松赛',
             'event': '半马',
@@ -18,7 +17,6 @@ def my_marathon(request):
             'notes': '首半马'
         },
         {
-            'year': '2024',
             'date': '2024-10-20',
             'name': '2024枣庄马拉松',
             'event': '半马',
@@ -27,13 +25,43 @@ def my_marathon(request):
             'notes': '半马 PB'
         },
         {
-            'year': '2024',
             'date': '2024-11-03',
             'name': '2024临沂马拉松',
             'event': '半马',
             'result': '未参加',
             'net_time': '-',
             'notes': '-'
+        },
+        {
+            'date': '2024-12-29',
+            'name': 'test1',
+            'event': '17公里',
+            'result': '还未开始',
+            'net_time': '-',
+            'notes': '-'
+        },
+        {
+            'date': '2025-01-01',
+            'name': 'test2',
+            'event': '10公里',
+            'result': '还未开始',
+            'net_time': '-',
+            'notes': '-'
         }
     ]
-    return render(request, 'marathon/my_marathon.html', {'races': races})
+
+    # 按日期排序，正序
+    races.sort(key=lambda x: x['date'])
+
+    # 按年份分组
+    year_group = {}
+    for race in races:
+        # 从日期中提取年份
+        year = race['date'][:4]
+        if year not in year_group:
+            year_group[year] = []
+        year_group[year].append(race)
+
+    return render(request, 'marathon/my_marathon.html', {
+        'year_group': year_group,
+    })
