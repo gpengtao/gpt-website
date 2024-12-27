5f223bbe7b80e32918a2b4c86abc71a32381a2cc
pengtao.geng <gpengtao@qq.com>
2024-12-27 19:19:48 +0800

    add test random

---
+++ learn/tests/test_random.py
@@ -0,0 +1,11 @@
+import random
+
+"""
+测试random.choice
+"""
+count = {"aaa": 0, "bbb": 0}
+for i in range(100000):
+    choice = random.choice(["aaa", "bbb"])
+    count[choice] += 1
+
+print(count)
