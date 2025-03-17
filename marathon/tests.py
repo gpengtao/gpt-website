1e6fbd8b0982a0b59157ef3aa612f3f174d4728d
pengtao.geng <gpengtao@qq.com>
2025-03-17 20:12:29 +0800

    时速配速转化

--- marathon/tests.py
+++ marathon/tests.py
@@ -1,5 +1,12 @@
-from django.test import TestCase
-
 # Create your tests here.
 
-for i in ra
+# 时速配速转化
+for speed in range(60, 201):
+    speed = speed / 10
+    total_time = 60.0 / speed
+    minutes = int(total_time)
+    remaining = total_time - minutes
+    seconds = round(remaining * 60)
+
+    # 格式化输出，确保秒数为两位数
+    print(f"{speed} km/h = {minutes}:{seconds:02d} min/km")
