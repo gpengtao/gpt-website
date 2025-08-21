e4a071612f0b870f0b26170adbeb7bf642f30c64
pengtao.geng <gpengtao@qq.com>
2025-08-21 09:12:20 +0800

    add

--- learn/tests/lang/test_time.py
+++ learn/tests/lang/test_time.py
@@ -1,8 +1,12 @@
 import time
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 timestamp = time.time()
-print(timestamp)
-print(int(timestamp))
+print(f"11111: {timestamp}")
+print(f"22222: {int(timestamp * 1000)}")
+print(f"33333: {int(timestamp)}")
+print(f"44444: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
 
-print(timedelta(days=1/24).total_seconds())
+print(f"55555: {timedelta(days=1 / 24).total_seconds()}")
+print(f"66666: {datetime.now()}")
+print(f"77777: {(datetime.now() - timedelta(days=1)).strftime('%Y%m%d')}")
